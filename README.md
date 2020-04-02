348005cdca9f67f0ad024e5d5a7ad29237133130
Bjørn Madsen <bm@aeons.dk>
2020-04-02 17:23:44 +0200

    Implement simple S3 signer

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple S3 signer
+
+This library will give you a presigned S3 URL, with very few options.
+
+## Example
+
+```typescript
+import { signedS3Url } from "simple-s3-signer";
+
+const signed = signedS3Url({
+    bucket: "mybucket",
+    key: "/my-object-key",
+    region: "us-east-1",
+    accessKeyId: "AWS_ACCESSS_KEY_ID",
+    secretAccessKey: "AWS_SECRET_ACCESS_KEY"
+});
+
+fetch(signed).then(...);
+```
---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import { hash, hmac } from "fast-sha256";
+
+export interface SignUrl {
+  bucket: string;
+  key: string;
+  region: string;
+  accessKeyId: string;
+  secretAccessKey: string;
+}
+
+const te = new TextEncoder();
+
+const encode = (s: string) => te.encode(s);
+
+const pad = (n: number | string) => ("00" + n).slice(-2);
+
+const formatDate = (timestamp: Date) => {
+  const year = timestamp.getUTCFullYear();
+  const month = pad(timestamp.getUTCMonth() + 1);
+  const day = pad(timestamp.getUTCDate());
+  const date = year + month + day;
+  const hours = pad(timestamp.getUTCHours());
+  const minutes = pad(timestamp.getUTCMinutes());
+  const seconds = pad(timestamp.getUTCSeconds());
+
+  return [date, date + "T" + hours + minutes + seconds + "Z"];
+};
+
+const formatScope = (date: string, region: string): string =>
+  `${date}/${region}/s3/aws4_request`;
+
+const uint8arrayToHex = (arr: Uint8Array): string =>
+  Array.from(arr)
+    .map(b => pad(b.toString(16)))
+    .join("");
+
+export const signedS3Url = (opts: SignUrl) => {
+  const host = `${opts.bucket}.s3.amazonaws.com`;
+
+  const [date, time] = formatDate(new Date());
+  const scope = formatScope(date, opts.region);
+
+  const queryString =
+    `X-Amz-Algorithm=AWS4-HMAC-SHA256&` +
+    `X-Amz-Credential=${encodeURIComponent(`${opts.accessKeyId}/${scope}`)}&` +
+    `X-Amz-Date=${time}&` +
+    `X-Amz-Expires=86400&` +
+    `X-Amz-SignedHeaders=host`;
+
+  const canonicalRequest =
+    `GET\n` +
+    `${opts.key}\n` +
+    `${queryString}\n` +
+    `host:${host}\n\n` +
+    `host\nUNSIGNED-PAYLOAD`;
+
+  const hashedCanonicalRequest = uint8arrayToHex(
+    hash(encode(canonicalRequest))
+  );
+
+  const stringToSign = `AWS4-HMAC-SHA256\n${time}\n${scope}\n${hashedCanonicalRequest}`;
+
+  const dateKey = hmac(encode(`AWS4${opts.secretAccessKey}`), encode(date));
+  const dateRegionKey = hmac(dateKey, encode(opts.region));
+  const dateRegionServiceKey = hmac(dateRegionKey, encode("s3"));
+  const signingKey = hmac(dateRegionServiceKey, encode("aws4_request"));
+
+  const signature = uint8arrayToHex(hmac(signingKey, encode(stringToSign)));
+
+  return `https://${host}${opts.key}?${queryString}&X-Amz-Signature=${signature}`;
+};
